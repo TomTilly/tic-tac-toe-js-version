0191ee101399d4204d14785ac5865c7450708a51
Tom Tilly <ny.prosource@gmail.com>
2015-07-31 21:19:38 -0400

    Add game object and renderBoard object

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,48 @@
 $(document).ready(function()
 {
-	
-	
-	
-	var gameBoard =
+	var gameOver = false;
+	var currentPlayer = 1;
+	var game =
 	{
 		board: 
 		[
 			[0, 0, 0],
 			[0, 0, 0],
 			[0, 0, 0]
-		]
+		],
+		gamesPlayed: '',
+		player1Wins: '',
+		player1Losses: '',
+		player2Wins: '',
+		player2Losses: '',
+		resetBoard: function()
+		{
+			for(var i = 0; i < this.board.length; i++)
+			{
+				for(var j = 0; j < this.board[i].length; j++)
+				{
+					this.board[i][j] = 0;
+				}
+			}
+		}
 	};
+	
+	function renderBoard(newMove, currentPlayer)
+	{
+		if(!gameOver){
+			if(currentPlayer === 1)
+			{
+
+			} else
+			{
+
+			}
+		} else
+		{
+			$('img').remove();
+		}
+	}
+	
 	
 	
 });
