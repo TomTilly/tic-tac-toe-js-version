ddeb6ef13e2728d3452aa7ea3379e78123f05156
Tom Tilly <ny.prosource@gmail.com>
2015-08-01 22:15:14 -0400

    Add renderBoard function and change data-location attributes

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function()
 {
+	
+
 	var gameOver = false;
+	$button = $('button');
 	var currentPlayer = 1;
-	var game =
+	var ticTacToe =
 	{
 		board: 
 		[
@@ -24,18 +27,32 @@ $(document).ready(function()
 					this.board[i][j] = 0;
 				}
 			}
+		},
+		updateBoard: function($newMove)
+		{
+			var coordinates = $newMove.attr('data-location').split('-');
+			if(currentPlayer === 1)
+			{
+				this.board[coordinates[0]][coordinates[1]] = 2;
+			} else
+			{
+				this.board[coordinates[0]][coordinates[1]] = 5;
+			}
 		}
 	};
 	
-	function renderBoard(newMove, currentPlayer)
+	// Render a recent move or clear the board if game has been completed
+	function renderBoard($newMove)
 	{
 		if(!gameOver){
 			if(currentPlayer === 1)
 			{
-				$(this).append('<img src="assets/x.png" />');
+				$newMove.append('<img src="assets/x.png" />');
+				currentPlayer = 2;
 			} else
 			{
-				$(this).append('<img src="assets/o.png" />');
+				$newMove.append('<img src="assets/o.png" />');
+				currentPlayer = 1;
 			}
 		} else
 		{
@@ -43,6 +60,42 @@ $(document).ready(function()
 		}
 	}
 	
+	// Check to see if a player has won or the game is a draw
+	function gameEndCheck()
+	{
+		
+		
+	}
 	
+	
+	// Event to start or reset a game
+	$button.on(
+		'click',
+		function()
+		{
+			$(this).hide();
+			$playerLabel = $('p');
+			$playerLabel.html("Player 1's turn");
+			$('#gameboard-wrapper').on(
+				'click',
+				'div[data-location]',
+				function() 
+				{
+					var $newMove = $(this);
+					if (!($newMove.is(':parent(img)'))) // If space hasn't already been clicked
+					{
+						ticTacToe.updateBoard($newMove);
+						renderBoard($newMove);
+						if(gameOver)
+						{
+							
+						} else
+						{
+							$playerLabel.html("Player " + currentPlayer + "'s turn");
+						}
+					}
+				});
+		});
+		
 	
 });
